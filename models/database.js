6e319e3c5a3db8800373ddf98257b1f3da0f195c
maulanadata <crypton1001@gmail.com>
2022-08-30 23:27:43 +0700

    change db remote

--- models/database.js
+++ models/database.js
@@ -1,20 +1,20 @@
 const mysql = require("mysql");
 
-const koneksi = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "ujian_nodejs",
-  multipleStatements: true
-})
-
 // const koneksi = mysql.createConnection({
-// 	host: "remotemysql.com",
-// 	user: "2KlNMOkc3b",
-// 	password: "0aNdhRWoyk",
-// 	database: "2KlNMOkc3b",
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "ujian_nodejs",
 //   multipleStatements: true
 // })
+
+const koneksi = mysql.createConnection({
+	host: "remotemysql.com",
+	user: "2KlNMOkc3b",
+	password: "0aNdhRWoyk",
+	database: "2KlNMOkc3b",
+  multipleStatements: true
+})
 
 koneksi.connect((err) => {
   if (err) throw err;
